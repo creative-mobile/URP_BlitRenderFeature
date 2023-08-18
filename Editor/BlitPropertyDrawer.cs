b6194fed56ccec0e6742c82c739007ef94bf13a2
Alex <alex.tyk@yandex.ru>
2023-08-18 17:26:52 +0300

    upd editor

--- Editor/BlitPropertyDrawer.cs
+++ Editor/BlitPropertyDrawer.cs
@@ -72,6 +72,7 @@ namespace Cyan {
 				}
 				EditorGUILayout.EndHorizontal();
 				EditorGUILayout.PropertyField(property.FindPropertyRelative("disableMSAA"));
+				EditorGUILayout.PropertyField(property.FindPropertyRelative("size"));
 			} else if (enumValue == (int)Blit.Target.RenderTextureObject) {
 				EditorGUILayout.PropertyField(property.FindPropertyRelative("dstTextureObject"));
 			}
